52080417e1f46d2224455ffb2fc934de0c273726
Molean <cnmolean@gmail.com>
2020-03-20 20:46:01 +0800

    Change directory structure

--- run/start.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-title ·þÎñ¶Ë
-::java -Xmx4G -jar spigot-1.14.4.jar
-java  -javaagent:authlib-injector.jar=https://skin.molean.com:444/api/yggdrasil -Xmx4G -jar spigot-1.15.2.jar nogui
-pause
-EXIT
